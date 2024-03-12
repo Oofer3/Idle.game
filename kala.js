de012a92d700e8bc88addcb7db6b7d1518e68d09
Jaan Laskar <jaan.laskar@tptlive.ee>
2024-03-12 12:21:18 +0200

    123

--- kala.js
+++ kala.js
@@ -1,181 +1,179 @@
-let money = 0;
-let fish = 0;
-let pricemultiplier = 1.3;
+// Try to load game state from local storage
+let loadedState = JSON.parse(localStorage.getItem("state"))
+let state = loadedState || {
+    money: 0,
+    fish: 0,
+    market: 1.5,
+    fisherman: 0,
+    boat: 0,
+    beach: 0,
+    greyfish: 0,
+    bottle: 0,
+    taxi: 0,
+}
+
+// Save game state to local storage every 5 seconds
+setInterval(() => {
+    localStorage.setItem("state", JSON.stringify(state));
+}, 5000);
+
+let prices = {
+    market: 222,
+    fisherman: 10,
+    boat: 800,
+    beach: 9998,
+    bottle: 92340,
+    taxi: 762034,
+}
+
+let multipliers = {
+    market: 0.2,
+    fisherman: 0.3,
+    boat: 0.3,
+    beach: 0.3,
+    bottle: 0.3,
+    taxi: 0.3,
+}
 
 function roundToTwoDecimalPlaces(num) {
     return Math.round(num * 100) / 100;
 }
 
-let fishDisplay = document.querySelector("#fish-count");
+let fishDisplay = document.querySelector("#state-fish");
 
 fishing = () => {
-    if (Math.random() < (0.02 + fishermans * 0.01)) {
-        fish++
-        fishDisplay.textContent = fish;
+    if (Math.random() < (0.02 + state.fisherman * 0.01)) {
+        state.fish++
+        fishDisplay.textContent = state.fish;
         console.log("leidsin kala")
     }
-
 }
 a = setInterval(fishing, 100)
 function sellFish() {
-    money += (fish * market) + (greyfish * market * 12)
-    fish = 0;
-    greyfish = 0;
-    fishDisplay.textContent = fish;
+    state.money += (state.fish * state.market) + (state.greyfish * state.market * 12)
+    state.fish = 0;
+    state.greyfish = 0;
+    fishDisplay.textContent = state.fish;
 }
-let marketprice = 222
-let market = 1.5;
-let marketamount = 0;
+
 function buyMarket() {
-    if (money >= marketprice) {
-        marketamount++
-        market = market * 1.2
-        money -= marketprice
-        marketprice = marketprice * pricemultiplier
-        console.log("Sa arendasid enda turgu:", marketamount)
+    // TODO näita kuidas need kaks rida teha funktsiooniks
+    let itemName = "market"
+    let price = prices[itemName] + prices[itemName] * multipliers[itemName] * state[itemName];
+    if (state.money >= price) {
+        state.market++
+        state.market = state.market * 1.2
+        state.money -= price
+        console.log("Sa arendasid enda turgu:", state.market)
         let marketDisplay = document.querySelector("#market");
-        marketDisplay.textContent = market;
-    } else { alert("sul on vaja " + marketprice + " münti"); }
+        marketDisplay.textContent = state.market;
+    } else { alert("sul on vaja " + price + " münti"); }
 
 }
-let fishermanprice = 10;
-let fishermans = 0;
+
 function buyFisherman() {
-    if (money >= fishermanprice && fishermans < 100) {
-        fishermans++;
-        money -= fishermanprice;
-        fishermanprice = fishermanprice * pricemultiplier;
-        console.log("Sa ostsid kalamehe:", fishermans);
-    } else if (fishermans >= 100) {
+    let itemName = "fisherman"
+    let price = prices[itemName] + prices[itemName] * multipliers[itemName] * state[itemName];
+    if (state.money >= price && state.fisherman < 100) {
+        state.fisherman++;
+        state.money -= price;
+        console.log("Sa ostsid kalamehe:", state.fisherman);
+    } else if (state.fisherman >= 100) {
         alert("You can only buy a maximum of 100 fishermen.");
     } else {
-        alert("You need " + fishermanprice + " coins.");
+        alert("You need " + price + " coins.");
     }
 }
-let boatprice = 800;
-let boats = 0;
+
 function buyBoat() {
-    if (money >= boatprice && boats < 10) {
-        boats++;
-        money -= boatprice;
-        boatprice = boatprice * pricemultiplier;
+    let itemName = "boat"
+    let price = prices[itemName] + prices[itemName] * multipliers[itemName] * state[itemName];
+    if (state.money >= price && state.boat < 10) {
+        state.boat++;
+        state.money -= price;
         clearInterval(a);
-        a = setInterval(fishing, Math.max(10, 100 / boats));
-    } else if (boats >= 10) {
-        alert("You can only buy a maximum of 10 boats.");
+        a = setInterval(fishing, Math.max(10, 100 / state.boat));
+    } else if (state.boat >= 10) {
+        alert("You can only buy a maximum of 10 boat.");
     } else {
-        alert("You need " + boatprice + " coins.");
+        alert("You need " + price + " coins.");
     }
 }
 
 function catchFish() {
-    fish++;
-    fishDisplay.textContent = fish;
+    state.fish++;
+    fishDisplay.textContent = state.fish;
 }
-let greyfish = 0;
-let beach = 0;
-let beachprice = 9998;
+
 function buybeach() {
-    if (money >= beachprice && beach < 100) {
-        beach++;
-        money -= beachprice;
-        beachprice = beachprice * pricemultiplier;
-        console.log("Ostsid ühe ranna:", beach);
-    } else if (beach >= 100) {
+    let itemName = "beach"
+    let price = prices[itemName] + prices[itemName] * multipliers[itemName] * state[itemName];
+    if (state.money >= price && state.beach < 100) {
+        state.beach++;
+        state.money -= price;
+        console.log("Ostsid ühe ranna:", state.beach);
+    } else if (state.beach >= 100) {
         alert("You can only buy a maximum of 100 beaches.");
     } else {
-        alert("You need " + beachprice + " coins.");
+        alert("You need " + price + " coins.");
     }
 }
 
 fishing1 = () => {
-    if (Math.random() < (0.00 + beach * 0.01)) {
-        greyfish++
+    if (Math.random() < (0.00 + state.beach * 0.01)) {
+        state.greyfish++
         console.log("leidsin hõbedase kala")
     }
 }
 
 b = setInterval(fishing1, 100)
-let bottleprice = 92340;
-let bottle = 0;
 function buyBottle() {
-    if (money >= bottleprice && bottle < 10) {
-        bottle++;
-        money -= bottleprice;
-        bottleprice = bottleprice * pricemultiplier;
+    let itemName = "bottle"
+    let price = prices[itemName] + prices[itemName] * multipliers[itemName] * state[itemName];
+    if (state.money >= price && state.bottle < 10) {
+        state.bottle++;
+        state.money -= price;
         clearInterval(b);
         b = setInterval(fishing1, Math.max(10, 100 / bottle));
     } else if (bottle >= 10) {
         alert("You can only buy a maximum of 10 bottles.");
     } else {
-        alert("You need " + bottleprice + " coins.");
+        alert("You need " + price + " coins.");
     }
 }
-let taxi = 0;
-let taxiprice = 762034;
 function orderTaxi() {
-    if (money >= taxiprice) {
-        taxi++;
-        money -= taxiprice;
-        taxiprice *= pricemultiplier;
+    let itemName = "taxi"
+    let price = prices[itemName] + prices[itemName] * multipliers[itemName] * state[itemName];
+    if (state.money >= price) {
+        state.taxi++;
+        state.money -= price;
 
         switch (taxi) {
-            case 1:
-                alert("Congrats, saad nüüd koju minna");
-                break;
-            case 2:
-                alert("Congrats, ma saan ka nüüd koju minna");
-                break;
-            case 3:
-                alert("Congrats, karu saab ka nüüd koju minna");
-                break;
-            case 4:
-                alert("Congrats, taksojuht saab nüüd puhkusele minna");
-                break;
-            case 7:
-                alert("Congrats, taksojuht ostsis Pariisi korteri");
-                break;
-            case 11:
-                alert("Congrats, taksojuht on reisinud juba kaheksasse eri riiki");
-                break;
-            case 16:
-                alert("Congrats, taksojuht lämbus rahahunniku alla ära, puhaku ta rahus");
-                break;
-            case 19:
-                alert("Keep going you are close ");
-                break;
-            default:
-                alert("Congrats, taksojuht saab nüüd kauem puhkusel olla");
-                break;
+            case 1: alert("Congrats, saad nüüd koju minna"); break;
+            case 2: alert("Congrats, ma saan ka nüüd koju minna"); break;
+            case 3: alert("Congrats, karu saab ka nüüd koju minna"); break;
+            case 4: alert("Congrats, taksojuht saab nüüd puhkusele minna"); break;
+            case 7: alert("Congrats, taksojuht ostsis Pariisi korteri"); break;
+            case 11: alert("Congrats, taksojuht on reisinud juba kaheksasse eri riiki"); break;
+            case 16: alert("Congrats, taksojuht lämbus rahahunniku alla ära, puhaku ta rahus"); break;
+            case 19: alert("Keep going you are close "); break;
+            default: alert("Congrats, taksojuht saab nüüd kauem puhkusel olla"); break;
         }
-    } else { alert("sul on vaja " + taxiprice + " raha"); }
+    } else { alert("sul on vaja " + price + " raha"); }
     updateDisplay();
 }
 
 function updateDisplay() {
-    const elements = {
-        money: roundToTwoDecimalPlaces(money),
-        fish: roundToTwoDecimalPlaces(fish),
-        market: roundToTwoDecimalPlaces(market),
-        fishermans: roundToTwoDecimalPlaces(fishermans),
-        boats: roundToTwoDecimalPlaces(boats),
-        beach: roundToTwoDecimalPlaces(beach),
-        greyfish: roundToTwoDecimalPlaces(greyfish),
-        bottle: roundToTwoDecimalPlaces(bottle),
-        taxi: roundToTwoDecimalPlaces(taxi),
-        marketprice: roundToTwoDecimalPlaces(marketprice),
-        fishermanprice: roundToTwoDecimalPlaces(fishermanprice),
-        boatprice: roundToTwoDecimalPlaces(boatprice),
-        beachprice: roundToTwoDecimalPlaces(beachprice),
-        bottleprice: roundToTwoDecimalPlaces(bottleprice),
-        taxiprice: roundToTwoDecimalPlaces(taxiprice),
-        marketamount: roundToTwoDecimalPlaces(marketamount)
-    };
-
-    Object.entries(elements).forEach(([key, value]) => {
-        const display = document.querySelector(`#${key}`);
+    Object.entries(state).forEach(([key, value]) => {
+        let display = document.querySelector(`#state-${key}`);
         if (display) {
-            display.textContent = value;
+            display.textContent = roundToTwoDecimalPlaces(value);
+        }
+    });
+    Object.entries(prices).forEach(([key, value]) => {
+        let display = document.querySelector(`#price-${key}`);
+        if (display) {
+            display.textContent = roundToTwoDecimalPlaces(value + prices[key] * multipliers[key] * state[key]);
         }
     });
 }
@@ -185,74 +183,3 @@ let music = new Audio("Zen_Idle.mp3");
 music.loop = true;
 music.volume = 0.1;
 music.play();
-
-/*
-let saveButton = document.querySelector("#save-button");
-saveButton.addEventListener("click", () => {
-    const saveData = {
-        money,
-        fish,
-        market,
-        fishermans,
-        boats,
-        beach,
-        greyfish,
-        bottle,
-        taxi,
-        marketprice,
-        fishermanprice,
-        boatprice,
-        beachprice,
-        bottleprice,
-        taxiprice,
-        marketamount
-    };
-    localStorage.setItem("saveData", JSON.stringify(saveData));
-});
-
-let loadButton = document.querySelector("#load-button");
-
-loadButton.addEventListener("click", () => {
-    const saveData = JSON.parse(localStorage.getItem("saveData"));
-    if (saveData) {
-        money = saveData.money;
-        fish = saveData.fish;
-        market = saveData.market;
-        fishermans = saveData.fishermans;
-        boats = saveData.boats;
-        beach = saveData.beach;
-        greyfish = saveData.greyfish;
-        bottle = saveData.bottle;
-        taxi = saveData.taxi;
-        marketprice = saveData.marketprice;
-        fishermanprice = saveData.fishermanprice;
-        boatprice = saveData.boatprice;
-        beachprice = saveData.beachprice;
-        bottleprice = saveData.bottleprice;
-        taxiprice = saveData.taxiprice;
-        marketamount = saveData.marketamount;
-    }
-});
-
-let autoSaveInterval = setInterval(() => {
-    const saveData = {
-        money,
-        fish,
-        market,
-        fishermans,
-        boats,
-        beach,
-        greyfish,
-        bottle,
-        taxi,
-        marketprice,
-        fishermanprice,
-        boatprice,
-        beachprice,
-        bottleprice,
-        taxiprice,
-        marketamount
-    };
-    localStorage.setItem("saveData", JSON.stringify(saveData));
-}, 60000); // Save 1 min
-*/
